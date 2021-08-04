b464a18d85fd85897b04544d075ade32e10ba045
eugene.kovko <eugene.kovko@tensquaregames.com>
2021-08-04 21:45:47 +0200

    acknowledgement the receive

--- chapter2/publisher.py
+++ chapter2/publisher.py
@@ -1,5 +1,6 @@
 import sys
 import pika
+from pika import spec
 
 
 credentials = pika.PlainCredentials("guest", "guest")
@@ -13,11 +14,30 @@ channel.exchange_declare(exchange="hello-exchange",
                          durable=True,
                          auto_delete=False)
 
+def confirm_handler(frame):
+    if type(frame.method) == spec.Confirm.SelectOk:
+        print("Channel in confirm mode")
+    elif type(frame.method) == spec.Basic.Nack:
+        if frame.method.delivery_tag in msg_ids:
+            print("Message is lost!!!")
+    elif type(frame.method) == spec.Basic.Ack:
+        print("Confirm received")
+        msg_ids.remove(frame.method.delivery_tag)
+
+
+msg_ids = []
+channel.confirm_delivery()
 msg = sys.argv[1]
 msg_props = pika.BasicProperties()
 msg_props.content_type = "text/plain"
 
-channel.basic_publish(body=msg,
+try:
+    channel.basic_publish(body=msg,
                       exchange="hello-exchange",
                       properties=msg_props,
                       routing_key="hola")
+    print("Message has been received")
+except:
+    print("Message has not been received")
+msg_ids.append(len(msg_ids) + 1)
+channel.close()
